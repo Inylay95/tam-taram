1979041e543133a6dfc9879e4088dc8e6160a88c
Inna <inylay_95@mail.ru>
2018-07-25 17:59:34 +0300

    making header

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,24 @@
 // Your code here...
+$('[data-fancybox]').fancybox();
+
+$('.your-class').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: 'unslick'
+        },
+
+    ]
+});
