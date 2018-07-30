20a1be3a9cbff2e62c9901e9da370b5aa93d5584
Inna <inylay_95@mail.ru>
2018-07-30 09:58:18 +0300

    button-play

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,6 +7,8 @@ $('.slider').slick({
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 1,
+    nextArrow:"<span class=\"arrow arrow_next\"><img src=\"img/ico__next-arrow.png\"></span>",
+    prevArrow:"<span class=\"arrow arrow_prev\"><img src=\"img/ico__prev-arrow.png\"></span>",
     responsive: [
         {
             breakpoint: 991,
